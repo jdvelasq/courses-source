677964f59234b42708c6a8f2d07177f9f30d4983
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-04-13 07:53:34 -0500

    APP

--- notebooks/sklearn/sgd/app/app.py
+++ notebooks/sklearn/sgd/app/app.py
@@ -1,18 +1,21 @@
 
+import json
 import pickle
 
-from flask import Flask, request, flash, render_template
+from flask import Flask, flash, render_template, request
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config["SECRET_KEY"] = "you-will-never-guess"
+
+# Nombres de las clases
+classnames = ["setosa", "versicolor", "virginica"]
 
 @app.route("/", methods=["GET", "POST"])
-@app.route("/index", methods=("GET", "POST"))
+@app.route("/index", methods=["GET", "POST"])
 def index():
 
-    if request.method == 'POST':
-        # Nombres de las clases
-        classnames = ['setosa', 'versicolor', 'virginica']
-        
+    if request.method == "POST":
+
         # Lee los valores de las cajas de texto de la interfaz
         sepal_length = float(request.form["sepal_length"])
         sepal_width = float(request.form["sepal_width"])
@@ -24,12 +27,35 @@ def index():
         with open("clf.pickle", "rb") as f:
             clf = pickle.load(f)
 
-        result = classnames[ clf.predict(X)[0] ]
-        
+        result = classnames[clf.predict(X)[0]]
+
     else:
-        result = ''
+        result = ""
 
     return render_template("index.html", result=result)
 
+
+###############################################################
+#
+# API
+#
+###############################################################
+@app.route("/api/clasify", methods=["GET"])
+def classify():
+    if "values" in request.args:
+        X = request.args["X"]
+        X = X.split(",")
+        X = [float(v) for v in X]
+        X = [X]
+        
+        with open("clf.pickle", "rb") as f:
+            clf = pickle.load(f)
+
+        result = classnames[clf.predict(X)[0]]
+        return result
+    
+    else:
+        return "Error: No values field provided."
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", debug=True)
